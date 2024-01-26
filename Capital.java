febc9eebe50d4191f9fdaaf647970f9b0a20f63a
Vishal Kumar <vishalkumarprajapati1881@gmail.com>
2024-01-26 17:32:45 +0530

    dfd

--- Capital.java
+++ Capital.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Captital {
+public class Capital {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the String: ");
